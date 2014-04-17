52f6b23b1dd1a0abe8e458aaa45b9b3b21e27f03
Evandro Carreira <evandro.carreira@gmail.com>
2014-04-17 14:32:13 -0300

    Modificações para exemplo e utilização de um efeito só pra Agência KR

--- js/jquery.slitslider.js
+++ js/jquery.slitslider.js
@@ -9,6 +9,11 @@
  * http://www.codrops.com
  */
 
+
+
+
+
+
 ;( function( $, window, undefined ) {
 	
 	'use strict';
@@ -674,3 +679,76 @@
 	};
 	
 } )( jQuery, window );
+
+ /* 
+ 	@Evandro Carreira - 11/04/2014
+ 	Adicionada a função que starta os slides direto no arquivo de inclusão do plugin
+ 	Assim fica mais fácil de incluir os arquivos e trabalhar com vários plugins ao mesmo tempo
+ */
+
+ var Page = (function() {
+
+ 	var $navArrows = $( '#nav-arrows' ),
+ 	$nav = $( '#nav-dots > span' ),
+ 	slitslider = $( '#slider' ).slitslider( {
+ 		onBeforeChange : function( slide, pos ) {
+
+ 			$nav.removeClass( 'nav-dot-current' );
+ 			$nav.eq( pos ).addClass( 'nav-dot-current' );
+
+ 		}
+ 	} ),
+
+ 	init = function() {
+
+ 		initEvents();
+
+ 	},
+initEvents = function() {
+
+		// add navigation events
+		$navArrows.children( ':last' ).on( 'click', function() {
+
+			slitslider.next();
+			return false;
+
+		} );
+
+		$navArrows.children( ':first' ).on( 'click', function() {
+			
+			slitslider.previous();
+			return false;
+
+		} );
+
+		$nav.each( function( i ) {
+
+			$( this ).on( 'click', function( event ) {
+				
+				var $dot = $( this );
+				
+				if( !slitslider.isActive() ) {
+
+					$nav.removeClass( 'nav-dot-current' );
+					$dot.addClass( 'nav-dot-current' );
+
+				}
+				
+				slitslider.jump( i + 1 );
+				return false;
+
+			} );
+			
+		} );
+
+	};
+
+	return { init : init };
+
+})();
+
+Page.init();
+
+/*
+Fim da função de startar os slides
+*/
